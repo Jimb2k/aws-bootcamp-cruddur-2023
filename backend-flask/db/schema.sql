e0cda99bfed86c134d33a322a1aa52ad66953bca
Jimb2k <jamal.tajjar@gmail.com>
2023-04-01 01:12:47 +0000

    second commit for database week-4

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -1,3 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-export CONNECTION_URL="postgresql://postgres:password@localhost:5432/cruddur"
+gp env | export CONNECTION_URL="postgresql://postgres:password@localhost:5432/cruddur"
+
+export PROD_CONNECTION_URL="postgresql://root:1.Kooora1*+09@cruddur-db-instance.cwfwd7m8vnbz.us-east-1.rds.amazonaws.com:5432/cruddur"
